108f956955e7cad7a78dce2e7f6563352d178f73
moh3a <aitabdelmalekma@gmail.com>
2023-11-01 20:12:29 +0100

    Revert back to just a readme

--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-export const SITE_NAME = "moh3a";
-export const SITE_TITLE = `Code Here | ${SITE_NAME}`;
-export const SITE_DESCRIPTION = "";
-
-export const SITE_URL = `https://moh3a.dev`;
--- todo.md
+++
@@ -1,9 +0,0 @@
-## todo
-
-- add dark theme
-
-## first blog posts
-
-- basics of programming in js/ts
-- create a video chat application
-- create an ecommerce with aliexpress
